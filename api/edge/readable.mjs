54d82ac2b423c2d36300e3456ccc1ae47cae1973
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-08-13 03:46:29 +0500

    Update dependency versions and add new API route
    Updated the version of dependency packages @lit-labs/ssr and lit to @ponomarevlad/lit-labs-ssr@3.1.5-ssr-readable-exports-npm-1 and @ponomarevlad/lit@2.8.0-ssr-readable-exports-npm-1 respectively. This change was due to the need to upgrade to the latest versions of these packages which include important bug fixes and new features.
    
    Also, added a new API route '/api/edge/readable' in index.html and added the corresponding server-side rendering script in a newly created file - api/edge/readable.mjs. This adds a new functionality for rendering readable streams on the server side as part of an ongoing effort to enhance application performance and responsiveness.

---
+++ api/edge/readable.mjs
@@ -0,0 +1,31 @@
+import {html} from 'lit';
+import {render} from '@lit-labs/ssr';
+import {
+  renderResultReadableStream
+} from '@lit-labs/ssr/lib/render-result-readable-stream.js';
+import {
+  pageTemplate
+} from '../../src/page-template.mjs';
+import '../../src/server-url.mjs';
+import {
+  urlFromRequest
+} from '../../src/utils.mjs';
+import importmap from '../../importmap.json';
+
+const headers = {'content-type': 'text/html'};
+
+const head = () => render(html`
+  <script src='/src/server-url.mjs' type='module'></script>
+`);
+
+const body = ({url = ''} = {}) => render(html`
+  <server-url url='${url}'></server-url>
+`);
+
+export default request => {
+  const url = urlFromRequest(request);
+  const result = pageTemplate({head, body, importmap, context: {url}});
+  return new Response(renderResultReadableStream(result), {headers});
+}
+
+export const config = {runtime: 'edge'};
